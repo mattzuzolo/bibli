9be98d927bc2053579d5131730f0aa3e6ebff745
Matt Zuzolo <mattzuzolo@outlook.com>
2018-09-26 22:12:53 -0400

    removed unneccessary spacing in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-
 class NavBar extends Component {
-
   render(){
-    console.log("Query state", this.props.searchQuery)
     return(
       <div className="div--nav-bar">
         <Link className="nav-item div--nav-bar-item nav-left logo" to="/profile">Bibli</Link>
         <form
           className="nav-search nav-item div--nav-bar-item nav-left" onSubmit={this.props.onSearchSubmit}>
-
             <input
               className="nav-search"
               value={this.props.searchQuery}
@@ -19,7 +15,6 @@ class NavBar extends Component {
               placeholder="Search for a book, author or ISBN">
             </input>
         </form>
-
 
         <Link className="nav-item div--nav-bar-item nav-right" to="/profile">Profile</Link>
       </div>
