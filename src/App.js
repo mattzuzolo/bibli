3e3ec99286c4fd18985e3a8116666ed4161c000c
mattzuzolo <mattzuzolo@outlook.com>
2018-09-29 11:50:43 -0400

    added comments to app.js. Also expanded external Google fetch volume

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ProfileContainer from "./components/containers/ProfileContainer"
 //Components
 import NavBar from "./components/NavBar"
 
+//Rails API endpoints
 const bookUrl = "http://localhost:3000/api/v1/books"
 const collectionsUrl = "http://localhost:3000/api/v1/collections"
 const bookCollectionUrl = "http://localhost:3000/api/v1/book_collections"
@@ -33,28 +34,31 @@ class App extends Component {
       .then(response => response.json())
       .then(collectionsArray => this.setState({collectionsArray}))
 
+    //fetch all the books to use rails API data when available (rather than google api)
     fetch(`http://localhost:3000/api/v1/books/`)
       .then(response => response.json())
       .then(allBooks => this.setState({allBooks}))
   }
 
-  //Update state to maintain controlled search form
+  //Update state to maintain controlled book search form
   onSearchQueryChange = (event) => {
     this.setState({ searchQuery: event.target.value })
   }
 
-  //Form submit event listener. Submission will trigger fetch to external Google books api
+  //Book search form submit event listener. Submission will trigger fetch to external Google books api
   onSearchSubmit = (event) => {
     event.preventDefault();
+    //save query at time of submission and convert string to be used in query string for external API call
     let submittedQuery = this.state.searchQuery
     let submittedQueryWithPlus = submittedQuery.trim().split(' ').join('+');
-    //&maxResults=40
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${submittedQueryWithPlus}`)
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${submittedQueryWithPlus}&maxResults=40`)
       .then(response => response.json())
+      //update state with the fetchedBooks and reset the searchQuery input field
       .then(fetchedBookArray => this.setState({
         fetchedBookArray: fetchedBookArray.items,
         searchQuery: ""
       }))
+      //Push users to the search result page after successfully fetching and setState
       .then(() => this.props.history.push(`/search/${submittedQueryWithPlus}`))
       .catch(error => {
         console.log("An error occurred during the fetch", error)
@@ -62,13 +66,15 @@ class App extends Component {
       })
   }
 
+  //Update state to maintain controlled newCollectionInput field
   onNewCollectionInputChange = (event) => {
     this.setState({newCollectionInput: event.target.value})
   }
 
+  //Event listener for create new Collection
   onNewCollectionInputSubmit = (event) => {
-    console.log("FORM SUBMITTED")
     event.preventDefault();
+    //save data that will be POSTed to rails api
     let name = this.state.newCollectionInput;
     let collectionPostBody = {
       user_id: this.state.currentUser.id,
@@ -82,7 +88,7 @@ class App extends Component {
       },
       body: JSON.stringify(collectionPostBody)
     }
-
+    //POST request happens here. Input field reset and new collection optimistically rendered to state.collectionsArray
     fetch(collectionsUrl, collectionPostConfig)
     this.setState({
       newCollectionInput: "",
@@ -93,10 +99,11 @@ class App extends Component {
   //Event listener on event list so user can visit see the specific collection
   onCollectionItemClick = (event, selectedCollection) => {
     this.setState({selectedCollection: selectedCollection})
+    //bring user to the apropriate collection on click
     this.props.history.push(`/collections/${selectedCollection.id}`)
   }
 
-  //Event listener for each card in a specific collection so User can get to individual book detail page
+  //Checks if the selectedBook is available locally. If not, creates a new DB entry from external Google Books api
   checkIfBookExists = (allBooks, selectedBook) => {
     return allBooks.find(book => book.google_id === selectedBook.id)
   }
@@ -106,7 +113,7 @@ class App extends Component {
 
     let foundBook = this.checkIfBookExists(this.state.allBooks, selectedBook);
 
-    //POST new book to DB if doesn't exist. Push user onward to detail page
+    //POST new book to DB if doesn't exist locally. Push user onward to detail page regardless.
     if(foundBook){
       return this.props.history.push(`/books/${foundBook.google_id}`)
     }
@@ -130,6 +137,7 @@ class App extends Component {
 
   }
 
+  //method to POST book to rails api
   postNewBook = (postBookBody) => {
     const postBookConfig = {
       Accept: "application/json",
@@ -139,16 +147,15 @@ class App extends Component {
       },
       body: JSON.stringify(postBookBody)
     }
-    // console.log("postBookConfig:", postBookConfig)
     return fetch(bookUrl, postBookConfig)
   }
 
+  //POST fetch that adds a new book to an existing collection
   onAddBookToCollectionSubmit = (event, book_id) => {
     let bookCollectionBody = {
       collection_id: parseInt(event.target.value),
       book_id,
     }
-
     let postBookCollectionConfig = {
       Accept: "application/json",
       method: "POST",
